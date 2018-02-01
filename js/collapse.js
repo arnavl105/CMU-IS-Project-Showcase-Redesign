7253884d7d933714329f7850dbbf1126c2f3aebe
arnavl105 <arnavl105@gmail.com>
2018-01-31 23:20:08 -0500

    SCM was not used for this project, commit simply for the sake of uploading to github

---
+++ js/collapse.js
@@ -0,0 +1,9 @@
+
+function toggleChevron(e) {
+	$(e.target)
+		.prev('#filterHeader')
+		.find("i.indicator")
+		.toggleClass('fa-caret-down fa-caret-right');
+	}
+	$('.sidebar').on('hidden.bs.collapse', toggleChevron);
+	$('.sidebar').on('shown.bs.collapse', toggleChevron);}
